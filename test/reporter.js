ea9e7e7441b1a6d13e833e08d9e3089a070f49c2
Lennart Pegel <github@justlep.net>
2021-07-25 23:27:20 +0200

    include stacktraces

--- test/reporter.js
+++ test/reporter.js
@@ -1,11 +1,12 @@
-const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+const {SpecReporter, StacktraceOption} = require('jasmine-spec-reporter');
 
 jasmine.getEnv().clearReporters(); // remove default reporter logs
 jasmine.getEnv().addReporter(
     new SpecReporter({
         // add jasmine-spec-reporter
         spec: {
-            displayPending: true
+            displayPending: true,
+            displayStacktrace: StacktraceOption.PRETTY
         }
     })
 );
